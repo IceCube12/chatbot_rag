97b05b769df0c6f38680a8c48f357f3c8d351268
Rovic Munoz <72641945+IceCube12@users.noreply.github.com>
2024-08-02 10:11:57 +0800

    Update ui.py
    added chat history

--- ui.py
+++ ui.py
@@ -8,20 +8,36 @@ os.environ["GRPC_VERBOSITY"] = "NONE"
 
 load_dotenv()
 
-def generate_rag_prompt(query, context):
-    escaped = context.replace("'","").replace('"', "").replace("\n"," ")
-    prompt = ("""
-    You are a helpful and informative bot that answers questions using text from the reference context included below. \
-    Be sure to respond in a complete sentence.\
-    You can use bulletpoints. \
-    However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
-    strike a friendly and converstional tone. \
-    If the context is irrelevant to the answer, you may ignore it.
-            QUESTION: '{query}'
-            CONTEXT: '{context}'
-              
-            ANSWER:
-            """).format(query=query, context=context)
+def generate_rag_prompt(query, context="", conversation_history=[]):
+    prompt = ""
+    if conversation_history:
+        for chat in conversation_history:
+            prompt += "You: " + chat["query"] + "\n"
+            prompt += "BOT: " + chat["answer"] + "\n"
+    if context:
+        escaped = context.replace("'","").replace('"', "").replace("\n"," ")
+        prompt += ("""
+        You are a helpful and informative bot that answers questions using text from the reference context included below. \
+        Be sure to respond in a complete sentence.\
+        You can use bulletpoints. \
+        However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+        strike a friendly and converstional tone. \
+        If the context is irrelevant to the answer, you may ignore it.
+                CONTEXT: '{context}'
+        """).format(context=context)
+    else:
+        prompt += ("""
+        You are a helpful and informative bot that answers questions. \
+        Be sure to respond in a complete sentence.\
+        You can use bulletpoints. \
+        However, you are talking to a non-technical audience, so be sure to break down complicated concepts and \
+        strike a friendly and converstional tone.
+        """)
+    prompt += ("""
+                QUESTION: '{query}'
+                  
+                ANSWER:
+                """).format(query=query)
     return prompt
 
 def get_relevant_context_from_db(query):
@@ -39,6 +55,23 @@ def generate_answer(prompt):
     answer = model.generate_content(prompt)
     return answer.text
 
+def on_click_callback():
+    query = st.session_state.query
+    use_custom_data = st.session_state.use_custom_data
+    if use_custom_data:
+        context = get_relevant_context_from_db(query)
+    else:
+        context = ""
+    prompt = generate_rag_prompt(query=query, context=context, conversation_history=st.session_state.chat_history)
+    answer = generate_answer(prompt=prompt)
+    st.session_state.chat_history.append({"query": query, "answer": answer})
+
+def initialize_session_state():
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+
+initialize_session_state()
+
 logo_path = r"assets\logo_dark.png"
 st.title("")
 col1, col2 = st.columns([1, 4])
@@ -48,18 +81,27 @@ with col2:
     st.header("AI Assistant")
 st.markdown("A helpful and informative bot that answers your questions regarding the PIA.")
 
-input_container = st.container()
-with input_container:
-    query = st.text_input("You:", value="")
+chat_placeholder = st.container()
+prompt_placeholder = st.form("chat-from")
 
-output_container = st.container()
-with output_container:
-    st.markdown("")
+with chat_placeholder:
+    for chat in st.session_state.chat_history:
+        st.write("You: " + chat["query"])
+        st.write("BOT: " + chat["answer"])
 
-if st.button("Ask"):
-    context = get_relevant_context_from_db(query)
-    prompt = generate_rag_prompt(query=query, context=context)
-    answer = generate_answer(prompt=prompt)
-    output_container.empty()
-    with output_container:
-        st.markdown("BOT: " + answer)
+with prompt_placeholder:
+    st.markdown("**Chat**")
+    cols = st.columns((6,1))
+    cols[0].text_input(
+        "Chat",
+        value="",
+        label_visibility="collapsed",
+        key="query",
+    )
+    
+    cols[1].form_submit_button(
+        "Ask",
+        type="primary",
+        on_click=on_click_callback,
+    )
+    cols[0].checkbox("Ask about PIA", key="use_custom_data")
